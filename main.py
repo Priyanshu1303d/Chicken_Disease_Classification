ace1ed9742cd5e6e9ea7b5255ff992af7c1e8ff7
Priyanshu1303d <priyanshu1303d@gmail.com>
2025-01-07 13:19:53 +0530

    Data Ingestion Complete

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline .stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
-STAGE_NAME = "Data Ingestion stage"
+STAGE_NAME = "Data Ingestion"
 
 if __name__ == "__main__":
     try:
@@ -10,6 +11,20 @@ if __name__ == "__main__":
         data_ingestion = DataIngestionTrainingPipeline()
         data_ingestion.main()
         logger.info(f">>>> stage {STAGE_NAME} completed <<<<<<<<<\n\nx==============x")
+
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+
+STAGE_NAME2 = "Prepare Base Model"
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>>> stage {STAGE_NAME2} started <<<<<<<")
+        data_ingestion = PrepareBaseModelTrainingPipeline()
+        data_ingestion.main()
+        logger.info(f">>>> stage {STAGE_NAME2} completed <<<<<<<<<\n\nx==============x")
 
     except Exception as e:
         logger.exception(e)
